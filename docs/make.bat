de6b25b84808624c0fc65e045526ec26c9c71e67
fredericks1982 <fredericks1982@gmail.com>
2026-07-09 16:59:58 +0000

    chore: replace Apache license boilerplate with SPDX headers
    Swap the 13-line Apache 2.0 notice in every source, test, config, docs,
    and workflow file for the two-line SPDX equivalent. The full license
    text remains in LICENSE.

--- docs/make.bat
+++ docs/make.bat
@@ -1,16 +1,5 @@
-@REM Copyright 2024 - GitHub user: fredericks1982
-
-@REM Licensed under the Apache License, Version 2.0 (the "License");
-@REM you may not use this file except in compliance with the License.
-@REM You may obtain a copy of the License at
-
-@REM     http://www.apache.org/licenses/LICENSE-2.0
-
-@REM Unless required by applicable law or agreed to in writing, software
-@REM distributed under the License is distributed on an "AS IS" BASIS,
-@REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@REM See the License for the specific language governing permissions and
-@REM limitations under the License.
+@REM SPDX-FileCopyrightText: 2024 - GitHub user: fredericks1982
+@REM SPDX-License-Identifier: Apache-2.0
 
 @ECHO OFF
 
